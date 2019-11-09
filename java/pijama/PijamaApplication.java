8a2c26d31e698c394f8713b5a13021627d6a5746
Administrator <Administrator@HQPRTZ5F1LIK1FK>
2019-11-09 09:39:53 +0700

    comit

---
+++ java/pijama/PijamaApplication.java
@@ -0,0 +1,13 @@
+package pijama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PijamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PijamaApplication.class, args);
+	}
+
+}
